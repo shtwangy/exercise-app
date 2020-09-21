c015b87a9b11d3599a9f8ad0d85f7528f17b0e79
shtwangy <shtwangy@gmail.com>
2020-09-21 18:15:53 +0900

    feat(src/app/video/video.component): when the video is stopped, retrieve the video play time and add the record to the database.
    Closes #9

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ExerciseLogService } from '../core/services/exercise-log/exercise-log.service';
 
 @Component({
   selector: 'app-video',
@@ -7,13 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoComponent implements OnInit {
   id = '60YEfkhmOOM';
+  startTime: number;
 
-  constructor() { }
+  constructor(
+    private exerciseLogService: ExerciseLogService
+  ) { }
 
   ngOnInit(): void {
     const tag = document.createElement('script');
     tag.src = 'https://www.youtube.com/iframe_api';
     document.body.appendChild(tag);
   }
+
+  onReady(event: YT.PlayerEvent): void {
+    this.startTime = new Date().getTime();
+  }
+
+  onStateChange(event: YT.OnStateChangeEvent): void {
+    if (event.data === YT.PlayerState.PLAYING ) {
+      this.startTime = new Date().getTime();
+    }
+    if (event.data === YT.PlayerState.ENDED || event.data === YT.PlayerState.PAUSED) {
+      // console.log('経過時間');
+      const exerciseTime = (new Date().getTime() - this.startTime) / 1000;
+      console.log(exerciseTime);
+      this.exerciseLogService.createExerciseLog(exerciseTime);
+      this.startTime = new Date().getTime();
+    }
+  }
+
 
 }
