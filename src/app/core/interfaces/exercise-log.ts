a29d2277b3a6a83f5bae8873ddbebac78ac28914
shtwangy <shtwangy@gmail.com>
2020-09-22 17:47:11 +0900

    feat(src/app/core/interfaces/exercise-log.ts): add id and uid.
    Issues #2

--- src/app/core/interfaces/exercise-log.ts
+++ src/app/core/interfaces/exercise-log.ts
@@ -1,6 +1,8 @@
 import * as firebase from 'firebase/app';
 
 export default interface ExerciseLog {
+  id?: string;
+  uid: string;
   created_at: firebase.firestore.Timestamp;
   updated_at: firebase.firestore.Timestamp;
   exercise_time: number;
