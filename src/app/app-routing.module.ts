8887f428b0c347735b9bea2171f0532fe2723d81
shtwangy <shtwangy@gmail.com>
2020-10-27 00:54:47 +0900

    feat(src/app/dashboard/dashboard.component): create dashboard component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './core/guards/auth/auth.guard';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'exercise', pathMatch: 'full' },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '', component: DashboardComponent, canActivate: [AuthGuard] },
   {
     path: 'users',
     loadChildren: () => import('./users/users.module').then( m => m.UsersModule),
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,11 +15,22 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { HeaderComponent } from './core/components/header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    NavComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
@@ -32,7 +43,14 @@ import { MatButtonModule } from '@angular/material/button';
     BrowserAnimationsModule,
     FlexLayoutModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    LayoutModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule
   ],
   providers: [
     { provide: REGION, useValue: 'asia-northeast1' }
