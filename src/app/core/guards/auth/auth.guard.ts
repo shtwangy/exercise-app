acd1468718d4ba36b9ff6a736a1908401c70a9c2
shtwangy <shtwangy@gmail.com>
2020-11-14 21:49:33 +0900

    feat(src/app/core/guards/auth): retrieve login info from local storage and perform automatic sign-in.
    Closes #10

--- src/app/core/guards/auth/auth.guard.ts
+++ src/app/core/guards/auth/auth.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
 import {AuthService} from '../../services/auth/auth.service';
+import {LocalStorageService} from '../../services/local-storage/local-storage.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +9,17 @@ import {AuthService} from '../../services/auth/auth.service';
 export class AuthGuard implements CanActivate {
   constructor(
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    private localStorageService: LocalStorageService
   ) {}
 
   canActivate( route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-    if (this.authService.isSignedIn) {
+    const login = this.localStorageService.retrieve('login');
+    if (login && !this.authService.isSignedIn) {
+      this.authService.signIn(login.email, login.password).subscribe(res => {
+        return true;
+      });
+    } else if (this.authService.isSignedIn) {
       return true;
     } else {
       this.router.navigate(['/users/sign_in']);
